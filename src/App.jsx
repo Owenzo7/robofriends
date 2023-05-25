1f284bc884cf50f8c9c10dbde685c4c551a29bf8
Owenzo7 <owenlee545@gmail.com>
2023-05-25 14:51:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Card from "./Components/card.component";
+
+function App() {
+  return (
+    <>
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/card.component.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+
+const Card = () => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow">
+    <img src="https://robohash.org/test" alt="robots" width={200} height={200}/>
+    <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+    </div>
+    </div>
+  )
+}
+
+export default Card
