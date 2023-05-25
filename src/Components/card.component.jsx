873fd03d9e67369f3dae3ce01a83d81b307cb4bc
Owenzo7 <owenlee545@gmail.com>
2023-05-25 15:01:08 +0300

    passed in props

--- src/Components/card.component.jsx
+++ src/Components/card.component.jsx
@@ -1,16 +1,22 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react"
+import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow">
-    <img src="https://robohash.org/test" alt="robots" width={200} height={200}/>
-    <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@gmail.com</p>
+    <div className=" tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img
+        src="https://robohash.org/test"
+        alt="robots"
+        width={200}
+        height={200}
+      />
+      <div>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
